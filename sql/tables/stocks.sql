7e9556545a11e86aa4d35c37618ddf8b24a8ddf0
Derek Anderson <derek@deberon.com>
2017-03-07 21:42:36 -0500

    Added SQl for stocks table.

---
+++ sql/tables/stocks.sql
@@ -0,0 +1,10 @@
+CREATE TABLE stocks (
+    symbol      varchar(8) PRIMARY KEY,
+    name        varchar(255) NOT NULL,
+    cik         integer NOT NULL,
+    city        varchar(75),
+    state       char(2),
+    zip         char(5),
+    street1     varchar(255),
+    street2     varchar(255)
+);
